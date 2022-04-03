8e56d28004cb0bab387938fd20545513f31ff348
Jean <billaudjean@gmail.com>
2022-04-03 19:56:52 +0200

    refactore(only 1 condition): create only one condition to resolve the case

--- kata/src/solution.php
+++ kata/src/solution.php
@@ -13,8 +13,24 @@ class Solution
 
     public function isLeapYear($year)
     {
-        if ($year % 400 === 0) return true;
-        if ($year % 4 === 0 && $year % 100 !== 0) return true;
-        return false;
+        return $this->isDivisibleBy400($year)
+            ? true
+            : $this->isDivisibleBy4($year) && !$this->isDivisibleBy100($year);
+    }
+
+    private function isDivisibleBy4($year)
+    {
+        return ($year % 4 === 0) ? true : false;
+    }
+
+    private function isDivisibleBy100($year)
+    {
+        return ($year % 100 === 0) ? true : false;
+    }
+
+    private function isDivisibleBy400($year)
+    {
+        return ($year % 400 === 0) ? true : false;
     }
 }
+
