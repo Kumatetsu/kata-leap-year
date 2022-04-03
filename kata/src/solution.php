793e7e1dfa2b0c6886754d06856b4b55ad9d5476
Jean <billaudjean@gmail.com>
2022-04-03 19:26:35 +0200

    green(2000): 2000 is considered as leapyear

--- kata/src/solution.php
+++ kata/src/solution.php
@@ -13,6 +13,7 @@ class Solution
 
     public function isLeapYear($year)
     {
+        if ($year % 400 === 0) return true;
         if ($year % 4 === 0 && $year % 100 !== 0) return true;
         return false;
     }
